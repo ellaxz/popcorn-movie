cfa13ca8f3ba4eb1e6e6358db4b389cb1fe8f0ea
ella <lookisella@gmail.com>
2023-07-04 20:39:44 +1000

    cleaning up data fetching

--- src/App.js
+++ src/App.js
@@ -84,12 +84,14 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController()
       async function fetchMovies() {
         try {
           setIsLoading(true)
           setError("")
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           )
 
           if (!res.ok)
@@ -97,10 +99,15 @@ export default function App() {
           const data = await res.json()
 
           if (data.Response === "False") throw new Error("movie not fouond")
+
           setMovies(data.Search)
+          setError("")
         } catch (err) {
           console.error(err.message)
-          setError(err.message)
+
+          if (err.name !== "AbortError") {
+            setError(err.message)
+          }
         } finally {
           setIsLoading(false)
         }
@@ -112,6 +119,9 @@ export default function App() {
         return
       }
       fetchMovies()
+      return function () {
+        controller.abort()
+      }
     },
     [query]
   )
